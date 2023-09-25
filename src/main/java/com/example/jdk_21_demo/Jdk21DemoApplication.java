f070261648f568e2cacfc407764b6a24da68946a
habin <danny9643@naver.com>
2023-09-25 12:47:27 +0900

    first commit

---
+++ src/main/java/com/example/jdk_21_demo/Jdk21DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdk_21_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk21DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk21DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdk_21_demo/Jdk21DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdk_21_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk21DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
